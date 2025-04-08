06196454662973af02aa75317f49127ff3c3f501
iamudyavar <aditya.udyavar@gmail.com>
2025-04-08 12:36:46 -0500

    Added dialogue box

--- Assets/Scripts/AgentDialogue.cs
+++ Assets/Scripts/AgentDialogue.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
+using TMPro;
 
 public class AgentDialogue : MonoBehaviour
 {
-    bool isPlayerDetected = false;
+    public GameObject dialogueCanvas;
+    public GameObject dialogueTemplate;
+    private bool isPlayerDetected = false;
+    private Transform detectedPlayer;
+    private GameObject currentDialogue;
+    private Camera playerCamera;
 
     void Update()
     {
@@ -10,9 +16,46 @@ public class AgentDialogue : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                Debug.Log("Hello, I am an agent!");
+                if (currentDialogue == null)
+                {
+                    CreateDialogue("Hello, I am an agent!");
+                }
             }
         }
+
+        if (currentDialogue != null && playerCamera != null)
+        {
+            PositionDialogue();
+        }
+    }
+
+    private void CreateDialogue(string text)
+    {
+        currentDialogue = Instantiate(dialogueTemplate, dialogueCanvas.transform);
+        currentDialogue.transform.SetParent(dialogueCanvas.transform, false);
+        currentDialogue.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = text;
+
+        if (detectedPlayer != null)
+        {
+            playerCamera = detectedPlayer.GetComponentInChildren<Camera>();
+            if (playerCamera == null)
+            {
+                Debug.LogWarning("MainCamera not found in detected player's hierarchy.");
+            }
+        }
+
+        currentDialogue.SetActive(true);
+    }
+
+    private void PositionDialogue()
+    {
+        // Spawn dialogue in front of agent
+        Vector3 agentPosition = transform.position;
+        Vector3 dialoguePosition = agentPosition;
+        dialogueCanvas.transform.position = dialoguePosition;
+        dialogueCanvas.transform.LookAt(playerCamera.transform);
+        dialogueCanvas.transform.Rotate(0, 180f, 0);
+        dialogueCanvas.transform.rotation = Quaternion.Euler(0, dialogueCanvas.transform.rotation.eulerAngles.y, 0);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -20,6 +63,7 @@ public class AgentDialogue : MonoBehaviour
         if (other.name == "Character")
         {
             isPlayerDetected = true;
+            detectedPlayer = other.transform;
         }
     }
 
@@ -28,6 +72,13 @@ public class AgentDialogue : MonoBehaviour
         if (other.name == "Character")
         {
             isPlayerDetected = false;
+            detectedPlayer = null;
+            playerCamera = null;
+            if (currentDialogue != null)
+            {
+                Destroy(currentDialogue);
+                currentDialogue = null;
+            }
         }
     }
 }
